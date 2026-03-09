b2ef5f0cae958a5bd83b9894d60c11884e76de3e
DrissiOmar <omar.drissi@esprit.tn>
2026-03-09 17:26:12 +0100

    feat(entity): introduce BaseEntity with JPA auditing fields

---
+++ backend/shipment-tracker/src/main/java/com/example/shipment_tracker/shared/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.example.shipment_tracker.shared;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE", insertable = false)
+    private LocalDateTime updatedAt;
+
+
+}
