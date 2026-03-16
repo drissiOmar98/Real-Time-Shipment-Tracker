7d27e139814006b0dd75573e2d67f9ba7088025f
DrissiOmar <omar.drissi@esprit.tn>
2026-03-16 21:15:47 +0100

    refactor(frontend): replace static layout with router-outlet and add dashboard route

--- frontend/shipment-ui/src/app/app.routes.ts
+++ frontend/shipment-ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Dashboard} from './layout/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+
+  {path: 'dashboard', component: Dashboard}
+
+];
--- frontend/shipment-ui/src/app/app.ts
+++ frontend/shipment-ui/src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CreateShipment} from './features/shipments/create-shipment/create-shipment';
-import {ShipmentGrid} from './features/shipments/shipment-grid/shipment-grid';
-import {UpdateShipment} from './features/shipments/update-shipment/update-shipment';
 import {Header} from './layout/header/header';
-import {Notification} from './shared/components/notification/notification';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CreateShipment, ShipmentGrid, UpdateShipment, Header, Notification],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
